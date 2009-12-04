8a3b34d2e51d930b60701859b74b1610be930e4e
Sebastien Rannou <rannou.sebastien@gmail.com>
2009-12-04 10:54:22 +0100

    Added error code for page fault ISR

--- includes/isr.h
+++ includes/isr.h
@@ -5,7 +5,7 @@
 ** Login   <rannou_s@epitech.net>
 ** 
 ** Started on  Sun Nov 29 19:18:10 2009 sebastien rannou
-** Last update Fri Dec  4 10:37:45 2009 sebastien rannou
+** Last update Fri Dec  4 10:53:37 2009 sebastien rannou
 */
 
 #ifndef __ISR_HH__
@@ -42,22 +42,22 @@ isr_install(void);
  * These symbols are implemented in start.s
  */
 
-extern void             isr_0(void);    /* Floating exception */
-extern void             isr_1(void);    /* Floating exception */
-extern void             isr_2(void);    /* Floating exception */
-extern void             isr_3(void);    /* Floating exception */
-extern void             isr_4(void);    /* Floating exception */
-extern void             isr_5(void);    /* Floating exception */
-extern void             isr_6(void);    /* Floating exception */
-extern void             isr_7(void);    /* Floating exception */
-extern void             isr_8(void);    /* Floating exception */
-extern void             isr_9(void);    /* Floating exception */
-extern void             isr_10(void);    /* Floating exception */
-extern void             isr_11(void);    /* Floating exception */
-extern void             isr_12(void);    /* Floating exception */
-extern void             isr_13(void);    /* Floating exception */
-extern void             isr_14(void);    /* Floating exception */
-extern void             isr_15(void);    /* Floating exception */
-extern void             isr_16(void);    /* Floating exception */
+extern void             isr_0(void);
+extern void             isr_1(void);
+extern void             isr_2(void);
+extern void             isr_3(void);
+extern void             isr_4(void);
+extern void             isr_5(void);
+extern void             isr_6(void);
+extern void             isr_7(void);
+extern void             isr_8(void);
+extern void             isr_9(void);
+extern void             isr_10(void);
+extern void             isr_11(void);
+extern void             isr_12(void);
+extern void             isr_13(void);
+extern void             isr_14(void);
+extern void             isr_15(void);
+extern void             isr_16(void);
 
 #endif /* __ISR_HH__ */
--- src/start.s
+++ src/start.s
@@ -87,23 +87,32 @@ isr_%1:
 
 %endmacro
 
-isr_noerr 0
-isr_noerr 1
-isr_noerr 2
-isr_noerr 3
-isr_noerr 4
-isr_noerr 5
-isr_noerr 6
-isr_noerr 7
-isr_noerr 8
-isr_noerr 9
-isr_noerr 10
-isr_noerr 11
-isr_noerr 12
-isr_noerr 13
-isr_noerr 14
-isr_noerr 15
-isr_noerr 16
+isr_noerr 0                     ; floating exception
+isr_noerr 1                     ; debug exceptions
+isr_noerr 2                     ; intel reserved
+isr_noerr 3                     ; breakpoint
+isr_noerr 4                     ; overflow
+isr_noerr 5                     ; bounds check
+isr_noerr 6                     ; invalid opcode
+isr_noerr 7                     ; coprocessor not available
+isr_noerr 8                     ; double fault
+isr_noerr 9                     ; coprocessor segment overrun
+isr_noerr 10                    ; invalid tss
+isr_noerr 11                    ; segment not present
+isr_noerr 12                    ; stack exception
+isr_noerr 13                    ; triple fault
+
+global  isr_14                  ; page fault
+        
+isr_14:
+
+        push BYTE 14
+        mov eax, cr2
+        push eax                ; Address that faulted
+        jmp isr_handler
+        
+isr_noerr 15                    ; intel reserved
+isr_noerr 16                    ; corprocessor error
 
 isr_handler:
         
